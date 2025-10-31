d91076f4e2c10de05229ed621d437dbf8906e720
blueoxygens <exboxer234@gmail.com>
2025-10-31 10:09:14 +0900

    [chore] 프로젝트 초기 세팅

---
+++ src/main/java/com/know_who_how/mydata_server/MydataServerApplication.java
@@ -0,0 +1,13 @@
+package com.know_who_how.mydata_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/know_who_how/mydata_server/MydataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.know_who_how.mydata_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
